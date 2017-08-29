12970ca90e5c2fa2174bc5e648d33e7b43545594
Weeruhputt Supsohmboon <59070162@it.kmitl.ac.th>
2017-08-29 22:28:20 +0700

    Add cmd-inj

---
+++ web/cmd-inj/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en-US" class="no-js">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+    <meta name="referrer" content="no-referrer">
+    <link rel='stylesheet' id='all-css-0' href='https://instagram-brand.com/wp-content/mu-plugins/jetpack/modules/simple-payments/simple-payments.css?m=1501705382g' type='text/css' media='all' />
+    <link rel='stylesheet' id='brc-styles-css' href='https://en.instagram-brand.com/wp-content/themes/ig-branding/prj-ig-branding/assets/styles/main-6cc808110c.css?ver=5.4.8.1' type='text/css' media='all' />
+</head>
+<style>
+    .form {
+        padding: 10px 10px 10px 10px;
+    }
+</style>
+<body class="home blog">
+    <div id="app">
+        <div class="route-content-wrapper" data-route="/">
+            <div class="home route-content">
+                <div class="home__wrapper page-container">
+                    <div class="home__column">
+                        <h1 class="home__title">Welcome to our<br>Pinger!!</h1>
+                        <div class="request-form__form">
+                            <form class="request-form form" action="index.php" method="post">
+                                <div class="signin-email underline">
+                                    <input name="host" type="text" placeholder="try google.com" style="text-align: center;">
+                                </div>
+                                <hr>
+                                <?php
+                                    if (isset($_POST['host'])) {
+                                        $host = $_POST['host'];
+                                        echo '<div class="form__row">'
+                                        .shell_exec('ping -c 2 '.$host)
+                                        .'</div>';
+                                    }
+                                ?>
+                                <div class="form__row">
+                                    <button type="submit" class="button button-bg-white">Ping!!</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
